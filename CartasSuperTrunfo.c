778b8cbc509bba4aa5332d48efcf5abef55b72a9
MaylonField <maylon_rc@hotmail.com>
2025-09-24 16:54:43 +0000

    Criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,27 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+    char estado[10];         //estado
+    char código[3];          //código da carta
+    char cidade[15];         //O nome da cidade
+    int  habitantes;         //O número de habitantes da cidade
+    float largura;           //A área da cidade em quilkometros quadrados
+    float produtobruto1;     // O produto Interno Bruto da cidade
+    int pontosturisticos;    // A quantidade de pontos Turísticos na cidade
 
+
+    char estado[10];         //estado
+    char código[3];          //código da carta
+    char cidade[15];         //O nome da cidade
+    int  habitantes;         //O número de habitantes da cidade
+    float largura;           // A área da cidade em quilkometros quadrados
+    float produtobruto2;     // O produto Interno Bruto da cidade
+    int Pontosturisticos;    // A quantidade de pontos Turísticos na cidade
+
+  
+  // Área para entrada de dados
+       printf("Seja bem Vindo ao SuperTrunfo");
+       printf("Digite Carta A ou B");
   // Área para exibição dos dados da cidade
 
 return 0;
