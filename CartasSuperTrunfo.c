eb599beac3d4f867906a73abcca2822821ab5f33
MaylonField <maylon_rc@hotmail.com>
2025-09-24 21:16:21 +0000

    lapidando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,30 +7,32 @@
 int main() {
 
   
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Área para definição das variáveis para armazenar as propriedades das cidades.
 
     char carta[1];                           //carta
     char estado[14] = "Massachusetts";       //estado
-    char codigo[10] = "A";                   //código da carta
+    char codigo[10] = "A01";                 //código da carta
     char cidade[15] = "Boston";              //O nome da cidade
     int  habitantes = 673458 ;               //O número de habitantes da cidade
-    float largura = 2321000 ;                //A área da cidade em quilkometros quadrados
+    float Área = 2321000 ;                   //A área da cidade em quilometros quadrados
     float PIB = 363000000 ;                  //O produto Interno Bruto da cidade
     int pontosturisticos = 20;               //A quantidade de pontos Turísticos na cidade
 
     // As Cartas B terminaram com numero 1 no final para diferenciar os dados.
+
     char carta1[1];                          //carta
     char estado1[30] = "Tokyo";              //estado
-    char codigo1[30] = "B" ;                 //código da carta 
+    char codigo1[30] = "B01" ;                 //código da carta 
     char cidade1[30] = "Tokyo";              //O nome da cidade
     int  habitantes1 = 14250000;             //O número de habitantes da cidade
-    float largura1 = 218900;                 // A área da cidade em quilkometros quadrados
+    float Área1 = 218900;                    // A área da cidade em quilometros quadrados
     float PIB1 = 113000000000;               // O produto Interno Bruto da cidade
     int pontosturisticos1 = 20;              // A quantidade de pontos Turísticos na cidade
 
   
        // Área para entrada de dados 
-       printf("Seja muito bem vindo(a) ao SuperTrunfo\n");  // um breve convite e motivação para o jogador.
+
+       printf("Seja muito bem vindo(a) ao SuperTrunfo\n");                // um breve convite e motivação para o jogador.
        printf("Um Jogo unico de cartas\n");
        printf("Preparado(a) para ser o grande vencedor(a)? \n");
        printf("Vamos começar!\n");
@@ -40,7 +42,7 @@ int main() {
        scanf("\n %c", carta);                                              // dado armazenado da carta A
        printf(" - Codigo: %s\n - Estado: %s\n", codigo, estado);           // selecão e impressão de Codigo e Estado.
        printf(" - Cidade: %s\n - Habitantes: %d\n", cidade, habitantes);   // selecão e impressão de Cidade e Habitantes.
-       printf(" - Largura: %.2f km²\n - PIB: R$ %.2f\n", largura, PIB);    // selecão e impressão de Tamanho por quilometro da cidade e PIB.
+       printf(" - Área: %.2f km²\n - PIB: R$ %.2f\n", Área, PIB);          // selecão e impressão de Tamanho por quilometro da cidade e PIB.
        printf(" - Pontos Turísticos: %d\n", pontosturisticos);             // selecão e impressão de Pontos Turisticos.
 
       // seleção e impressão da carta B
@@ -48,7 +50,7 @@ int main() {
        scanf("\n %c", carta1);                                             // dado armazenado da carta B.
        printf(" - Codigo: %s\n - Estado: %s\n", codigo1, estado1);         // selecão e impressão de Codigo e Estado.
        printf(" - Cidade: %s\n - Habitantes: %d\n", cidade1, habitantes1); // selecão e impressão  de Cidade e Habitantes.
-       printf(" - Largura: %.2f km²\n - PIB: R$%.2f\n", largura1, PIB1);   // selecão e impressão de Tamanho por quilometro e PIB.
+       printf(" - Área: %.2f km²\n - PIB: R$%.2f\n", Área1, PIB1);         // selecão e impressão de Tamanho por quilometro e PIB.
        printf(" - Pontos Turísticos: %d\n", pontosturisticos1);            // selecão e impressão de Pontos Turísticos.
 
 
